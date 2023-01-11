855afd17df03c772e887ca5b87643c1417c965bf
Yusr1l <118591666+Yusr1l@users.noreply.github.com>
2023-01-11 13:58:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ const getPassword = () => {
 const savePassword = () => {
   document.title = password.value
   saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`password saya: ${document.title}`))
-  saveButton.setAttribute('download', 'MyPWGenerator NAME.txt')
+  saveButton.setAttribute('download', 'MyPWGenerator Saya.txt')
 }
